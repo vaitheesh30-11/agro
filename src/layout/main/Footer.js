3b2712acf2464c1df6ed723426ef58f7cae2b499
vaitheesh30-11 <vaitheeshvighnesh@gmail.com>
2025-05-08 10:12:24 +0530

    Update Footer.js

--- src/layout/main/Footer.js
+++ src/layout/main/Footer.js
@@ -3,11 +3,6 @@ import { FaHeart } from 'react-icons/fa';
 function Footer() {
   return (
     <footer>
-      // <small>
-      //   &copy; {new Date().getFullYear()} made with{' '}
-      //   <FaHeart /> by -{' '}
-      //   Team AgroCare
-      // </small>
     </footer>
   );
 }
